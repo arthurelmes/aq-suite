be4ce977795eed9dbe5668ee200d327ce2e23ccc
Arthur Elmes <arthur.elmes@gmail.com>
2025-01-05 18:22:30 -0500

    Add remaning axes to plot, fix x-axis labels.

--- aq_suite/plot_aq.py
+++ aq_suite/plot_aq.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os.path as op
 from matplotlib.dates import DateFormatter, AutoDateLocator
 
-TEMP_INPUT = "/Users/arthurelmes/data/aq-suite/co2-pm-log-20241220T163443.txt"
+TEMP_INPUT = "/Users/arthurelmes/data/aq-suite/co2-pm-log-20250105T153240.txt"
 
 
 def main():
@@ -14,19 +14,30 @@ def main():
     mpl.rcParams['xtick.labelsize'] = 6  # Reduce font size for x-axis ticks
     mpl.rcParams['ytick.labelsize'] = 8  # Optionally for y-axis ticks
 
-    fig, (ax_0, ax_1) = plt.subplots(2)
-    ax_0.plot(data.measurement_time, data.co2)
-    ax_0.set_xlabel("datetime")
-    # ax_0.set_xticks(min(data.measurement_time), max(data.measurement_time))
+    fig, axes = plt.subplots(4)
     locator = AutoDateLocator()
-    formatter = DateFormatter("%Y-%m-%d %H:%M")
-    ax_0.xaxis.set_major_locator(locator)
-    ax_0.xaxis.set_major_formatter(formatter)
+    formatter = DateFormatter("%Y-%m-%d %H:%M:%S")
 
-    ax_0.set_ylabel("CO2 (PPM)")
-    ax_1.plot(data.measurement_time, data.temp_c)
-    ax_1.set_xlabel("datetime")
-    ax_1.set_ylabel("Temp (C)")
+    for axis, var in zip(axes, ["co2", "temp_c", "pm2.5", "pm10"]):
+        axis.plot(data.measurement_time, data[var])
+        axis.set_xlabel("datetime")
+        axis.set_ylabel(var)
+        axis.xaxis.set_major_locator(locator)
+        axis.xaxis.set_major_formatter(formatter)
+
+    # ax_0.plot(data.measurement_time, data.co2)
+    # ax_0.set_xlabel("datetime")
+    # ax_0.set_ylabel("CO2 (PPM)")
+    # ax_0.xaxis.set_major_locator(locator)
+    # ax_0.xaxis.set_major_formatter(formatter)
+
+    # ax_1.plot(data.measurement_time, data.temp_c)
+    # ax_1.set_xlabel("datetime")
+    # ax_1.set_ylabel("Temp (C)")
+    # ax_1.xaxis.set_major_locator(locator)
+    # ax_1.xaxis.set_major_formatter(formatter)
+
+
     out_plot_path = op.join(op.dirname(TEMP_INPUT), op.basename(TEMP_INPUT).replace(".txt", "_plot.pdf"))
 
     fig.autofmt_xdate()
