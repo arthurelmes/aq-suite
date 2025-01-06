06d5af8f01c4a0289cddc23a09b3e0025797e3e4
Arthur Elmes <arthur.elmes@gmail.com>
2025-01-06 06:55:03 -0500

    Update pre-commit for formatting, adjust logging output.

--- aq_suite/aqs.py
+++ aq_suite/aqs.py
@@ -1,24 +1,18 @@
-import pprint
-import time
-from datetime import datetime
-from datetime import timedelta
-
-from sds011 import SDS011
-from typing import Dict
-
 import argparse
 import logging
+import os.path as op
 import pprint
 import time
 from datetime import datetime, timedelta
 from typing import Dict, List
-import os.path as op
+
 import mh_z19 as m19
+from sds011 import SDS011
 
 FILL_VALUE = -999.0
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
@@ -35,7 +29,7 @@ def parse_args() -> Dict:
         "--log_dir_path",
         type=str,
         help="[Required] Full path to output dir for logfiles.",
-        required=True
+        required=True,
     )
     parser.add_argument(
         "-p",
@@ -74,7 +68,10 @@ def parse_args() -> Dict:
 
 def measure_co2_temp() -> Dict:
     raw_reading = m19.read_all()
-    co2_temp = {"co2": raw_reading.get("co2", FILL_VALUE), "temp_c": raw_reading.get("temperature", FILL_VALUE)}
+    co2_temp = {
+        "co2": raw_reading.get("co2", FILL_VALUE),
+        "temp_c": raw_reading.get("temperature", FILL_VALUE),
+    }
     return co2_temp
 
 
@@ -87,8 +84,9 @@ def measure_pm() -> Dict:
             pm[pm_val[0]] = reading[pm_val[1]]
         except IndexError:
             pm[pm_val[0]] = FILL_VALUE
-            
+
     return pm
+
 
 def append_data_to_file(file_name: str, datum: str, usecols: List) -> None:
     write_cols = [datum["measurement_time"].strftime("%Y-%m-%dT%H:%M:%S")]
@@ -98,7 +96,7 @@ def append_data_to_file(file_name: str, datum: str, usecols: List) -> None:
         f.write(write_cols)
         f.write("\n")
 
-        
+
 def add_log_file_header(file_path: str, notes: str, cols: List, config: Dict) -> None:
     """Create log file header, including any user-specified notes."""
     with open(file_path, "w") as f:
@@ -108,31 +106,45 @@ def add_log_file_header(file_path: str, notes: str, cols: List, config: Dict) ->
         f.write("\n\n")
         f.write(",".join(cols))
 
-         
+
 if __name__ == "__main__":
     args = parse_args()
 
-    formatted_time = datetime.strftime(datetime.now(), '%Y%m%dT%H%M%S')
+    formatted_time = datetime.strftime(datetime.now(), "%Y%m%dT%H%M%S")
     log_file_path = op.join(args["log_dir_path"], f"co2-pm-log-{formatted_time}.txt")
-    half_sleep_secs = int(args["ping_interval"]/2)
+    half_sleep_secs = int(args["ping_interval"] / 2)
     data_cols = ["measurement_time", "co2", "temp_c", "pm2.5", "pm10"]
-                    
+
     args["start_datetime"] = datetime.now() + timedelta(hours=args["start_delay_hours"])
-    args["end_datetime"] = args["start_datetime"] + timedelta(hours=args["logging_time_hours"])
+    args["end_datetime"] = args["start_datetime"] + timedelta(
+        hours=args["logging_time_hours"]
+    )
 
     pretty_args = pprint.pformat(args)
     logger.info("Running with the following configuration:\n%s", pretty_args)
-    add_log_file_header(log_file_path, args["notes"], cols=data_cols, config=pretty_args)
+    add_log_file_header(
+        log_file_path, args["notes"], cols=data_cols, config=pretty_args
+    )
 
+    logger.info("measurement_time\tco2\ttemp_c\tpm2.5\tpm10")
     while datetime.now() < args["end_datetime"]:
         if args["start_datetime"] <= datetime.now():
             co2_pm_datum = measure_co2_temp()
-            logger.debug("\nCO2 concentration (PPM): %f\nTemperature (C) %f", co2_pm_datum["co2"], co2_pm_datum["temp_c"])
             time.sleep(half_sleep_secs)
 
             co2_pm_datum.update(measure_pm())
             co2_pm_datum["measurement_time"] = datetime.now()
-            logger.debug("\nPM2.5 concentration: %f\nPM10 concentration: %f\n", co2_pm_datum["pm2.5"], co2_pm_datum["pm10"])
+            logger.info(
+                "\t".join(
+                    [
+                        co2_pm_datum["measurement_time"],
+                        co2_pm_datum["co2"],
+                        co2_pm_datum["temp_c"],
+                        co2_pm_datum["pm2.5"],
+                        co2_pm_datum["pm10"],
+                    ]
+                )
+            )
 
             append_data_to_file(log_file_path, co2_pm_datum, data_cols)
             time.sleep(half_sleep_secs)
